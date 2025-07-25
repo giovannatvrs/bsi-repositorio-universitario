bafc5b26f80b5085fb2bb08cfc6fe6d413537761
Giovanna <giovannatvrs@outlook.com>
2025-07-25 06:17:37 -0300

    springboot

---
+++ repositorio-universitario/src/main/java/com/example/repositorio_universitario/RepositorioUniversitarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.repositorio_universitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioUniversitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioUniversitarioApplication.class, args);
+	}
+
+}
---
+++ repositorio-universitario/src/test/java/com/example/repositorio_universitario/RepositorioUniversitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repositorio_universitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioUniversitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
